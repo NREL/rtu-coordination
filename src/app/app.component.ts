9d4e601efe753e87e61134cde052207df571c7b5
Alex Swindler <Alex.Swindler@nrel.gov>
2020-08-11 11:15:02 -0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,461 @@
-import { Component } from '@angular/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import * as am4core from '@amcharts/amcharts4/core';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+// import am4themes_themes_dark from '@amcharts/amcharts4/themes/dark.js';
+import { AfterViewInit, Component, HostListener, NgZone, OnDestroy, OnInit } from '@angular/core';
+import { MatSliderChange } from '@angular/material/slider';
+import { Papa } from 'ngx-papaparse';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
+
+// am4core.useTheme(am4themes_themes_dark);
+am4core.useTheme(am4themes_animated);
+
+type ViewType = 'rtu' | 'ev';
+
+export interface Record {
+  Time: Date;
+  'Bldg (kW) UnCoord': number;
+  'Peak Running (kW) UnCoord': number;
+  'Bldg RTU (kW) UnCoord': number;
+  'Bldg Other (kW) UnCoord': number;
+  'RTU On Count UnCoord': number;
+  'RTU 1 State UnCoord': number;
+  'RTU 2 State UnCoord': number;
+  'RTU 3 State UnCoord': number;
+  'RTU 4 State UnCoord': number;
+  'RTU 5 State UnCoord': number;
+  'RTU 6 State UnCoord': number;
+  'Bldg + EV (kW) UnCoord': number;
+  'Bldg (kW) Coord': number;
+  'Peak Running (kW) Coord': number;
+  'Bldg RTU (kW) Coord': number;
+  'Bldg Other (kW) Coord': number;
+  'RTU On Count Coord': number;
+  'RTU 1 State Coord': number;
+  'RTU 2 State Coord': number;
+  'RTU 3 State Coord': number;
+  'RTU 4 State Coord': number;
+  'RTU 5 State Coord': number;
+  'RTU 6 State Coord': number;
+  'Port 1 (kW)': number;
+  'Port 2 (kW)': number;
+  'Bldg + EV (kW) Coord': number;
+  'Bldg + EV + ESS (kW) Coord': number;
+  'Peak EV (kW) Coord': number;
+  'Peak ESS (kW) Coord': number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rtu-coordination';
+export class AppComponent implements AfterViewInit, OnDestroy, OnInit {
+  duration = 60;
+  interval: number | undefined;
+  playing = false;
+  timeIndex = {
+    current: 0,
+    min: 0,
+    max: 1438
+  };
+  rtuData: Record[] = [];
+  active: string;
+  view: ViewType = 'rtu';
+  leftStyle: string;
+  rightStyle: string;
+  private bars = [];
+  private costs = [];
+  private charts: am4charts.XYChart[] = [];
+  private dataReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
+  private $dataReady = this.dataReady.asObservable();
+  private peaks = [];
+  private peakUncoord = new Subject<string>();
+  private $peakUncoord = this.peakUncoord.asObservable().pipe(distinctUntilChanged());
+  private peakCoord = new Subject<string>();
+  private $peakCoord = this.peakCoord.asObservable().pipe(distinctUntilChanged());
+
+  constructor(
+    private papa: Papa,
+    private zone: NgZone
+  ) {
+    const view = localStorage.getItem('view');
+    if (['rtu', 'ev'].includes(view)) {
+      this.view = view as ViewType;
+    }
+  }
+
+  @HostListener('document:keydown.Space')
+  handleSpaceKey(): void {
+    this.togglePlaying();
+  }
+
+  @HostListener('document:keydown.ArrowLeft')
+  handleArrowLeftKey(): void {
+    if (this.timeIndex.current <= this.timeIndex.min) {
+      this.timeIndex.current = this.timeIndex.max;
+    } else {
+      --this.timeIndex.current;
+    }
+    this.update(this.timeIndex.current);
+  }
+
+  @HostListener('document:keydown.ArrowRight')
+  handleArrowRightKey(): void {
+    if (this.timeIndex.current >= this.timeIndex.max) {
+      this.timeIndex.current = this.timeIndex.min;
+    } else {
+      ++this.timeIndex.current;
+    }
+    this.update(this.timeIndex.current);
+  }
+
+  onSliderChange($event: MatSliderChange): void {
+    this.update($event.value);
+  }
+
+  togglePlaying(): void {
+    this.playing = !this.playing;
+    if (this.playing) {
+      this.interval = window.setInterval(() => {
+        if (this.timeIndex.current === this.timeIndex.max) {
+          this.timeIndex.current = this.timeIndex.min;
+        } else {
+          ++this.timeIndex.current;
+        }
+        this.update(this.timeIndex.current);
+      }, this.duration / (this.timeIndex.max - this.timeIndex.min) * 1000);
+    } else {
+      clearInterval(this.interval);
+    }
+  }
+
+  createChart(id: 'chart-uncoord' | 'chart-coord', data: {}[]): void {
+    const chart = am4core.create(id, am4charts.XYChart);
+
+    const topContainer = chart.chartContainer.createChild(am4core.Container);
+    topContainer.layout = 'absolute';
+    topContainer.toBack();
+    topContainer.marginTop = -15;
+    topContainer.paddingBottom = 5;
+    topContainer.width = am4core.percent(100);
+
+    const title = topContainer.createChild(am4core.Label);
+    title.align = 'left';
+    title.fontSize = 36;
+    title.text = id === 'chart-uncoord' ? 'BASELINE' : 'IMPROVED';
+    title.dx = 40;
+
+    // const cost = topContainer.createChild(am4core.Label);
+    // cost.align = 'center';
+    // cost.paddingTop = 20;
+    // cost.text = 'COST';
+    // this.costs.push(cost);
+
+    const peak = topContainer.createChild(am4core.Label);
+    peak.align = 'right';
+    peak.paddingTop = 20;
+    peak.text = 'PEAK DEMAND';
+    this.peaks.push(peak);
+
+    chart.data = data;
+    chart.paddingRight = 24;
+    // chart.colors.list = [
+    //   am4core.color('#434348'),
+    //   id === 'chart-uncoord' ? am4core.color('#283890') : am4core.color('#3ab54a')
+    // ];
+    chart.colors.list = [
+      am4core.color('#3ab54a'),
+      am4core.color('#283890')
+    ];
+
+    const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.dateFormats.setKey('hour', 'ha');
+    dateAxis.periodChangeDateFormats.setKey('hour', 'ha');
+
+    dateAxis.renderer.grid.template.location = 0;
+    dateAxis.renderer.labels.template.dy = -8;
+    dateAxis.renderer.minGridDistance = 60;
+    // dateAxis.title.text = 'TIME (JULY 3RD)';
+    // dateAxis.title.marginTop = -10;
+    // dateAxis.title.marginBottom = -5;
+
+    const bar = dateAxis.axisRanges.create();
+    bar.date = this.rtuData[0].Time;
+    bar.grid.stroke = am4core.color('red');
+    bar.grid.strokeWidth = 2;
+    bar.grid.strokeOpacity = 1;
+    this.bars.push(bar);
+
+    const valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+    valueAxis.renderer.maxWidth = 22;
+    valueAxis.min = 0;
+    valueAxis.title.text = 'kW';
+    valueAxis.title.dx = -12;
+    valueAxis.renderer.labels.template.dx = -20;
+    valueAxis.tooltip.disabled = true;
+
+    if (this.view === 'rtu') {
+      valueAxis.max = this.rtuData[this.rtuData.length - 1]['Peak Running (kW) UnCoord'];
+    } else if (this.view === 'ev') {
+      valueAxis.max = this.rtuData[this.rtuData.length - 1]['Peak EV (kW) Coord'];
+    }
+
+    // const otherSeries = chart.series.push(new am4charts.LineSeries());
+    // otherSeries.dataFields.dateX = 'date';
+    // otherSeries.dataFields.valueY = 'base';
+    // otherSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+    // otherSeries.name = 'BASE LOAD';
+
+    if (this.view === 'rtu') {
+      const rtuSeries = chart.series.push(new am4charts.LineSeries());
+      rtuSeries.dataFields.dateX = 'date';
+      rtuSeries.dataFields.valueY = 'rtu';
+      rtuSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+      rtuSeries.name = 'TOTAL LOAD';
+    } else if (this.view === 'ev') {
+      if (id === 'chart-uncoord') {
+        const evSeries = chart.series.push(new am4charts.LineSeries());
+        evSeries.dataFields.dateX = 'date';
+        evSeries.dataFields.valueY = 'ev';
+        evSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+        evSeries.name = 'BLDG + EV';
+      } else {
+        const essSeries = chart.series.push(new am4charts.LineSeries());
+        essSeries.dataFields.dateX = 'date';
+        essSeries.dataFields.valueY = 'ess';
+        essSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+        essSeries.name = 'BLDG + EV + ESS';
+      }
+    }
+
+    // const evSeries = chart.series.push(new am4charts.LineSeries());
+    // evSeries.dataFields.dateX = 'date';
+    // evSeries.dataFields.valueY = 'ev';
+    // evSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+    // evSeries.name = 'BLDG + EV';
+    //
+    // const essSeries = chart.series.push(new am4charts.LineSeries());
+    // essSeries.dataFields.dateX = 'date';
+    // essSeries.dataFields.valueY = 'ess';
+    // essSeries.tooltipText = '{valueY.formatNumber(\'0.0\')} kW';
+    // essSeries.name = 'BLDG + EV + ESS';
+
+    // const scrollbarX = new am4charts.XYChartScrollbar();
+    // scrollbarX.rtuSeries.push(rtuSeries);
+    // chart.scrollbarX = scrollbarX;
+
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.behavior = 'none';
+    chart.cursor.events.on('cursorpositionchanged', ev => {
+      if (id === 'chart-uncoord' && this.active === 'chart-uncoord') {
+        this.updateCursor(1, {
+          x: ev.target.xPosition,
+          y: ev.target.yPosition
+        });
+      } else if (id === 'chart-coord' && this.active === 'chart-coord') {
+        this.updateCursor(0, {
+          x: ev.target.xPosition,
+          y: ev.target.yPosition
+        });
+      }
+    });
+
+    chart.legend = new am4charts.Legend();
+    chart.legend.contentAlign = 'right';
+    chart.legend.fontSize = 12;
+    chart.legend.marginBottom = -15;
+    chart.legend.marginTop = -25;
+    chart.legend.itemContainers.template.clickable = false;
+    chart.legend.itemContainers.template.focusable = false;
+    chart.legend.itemContainers.template.cursorOverStyle = am4core.MouseCursorStyle.default;
+
+    this.charts.push(chart);
+    this.updatePeaks(0);
+  }
+
+  ngOnInit(): void {
+    this.papa.parse('assets/rtu.csv', {
+      download: true,
+      dynamicTyping: true,
+      header: true,
+      skipEmptyLines: true,
+      complete: result => {
+        this.rtuData = result.data.map(record => {
+          record.Time = new Date(record.Time);
+          return record;
+        });
+        this.dataReady.next(true);
+        this.dataReady.complete();
+        this.updateStyles(0);
+      }
+    });
+
+    this.$peakUncoord.subscribe(value => {
+      this.peaks[0].text = `PEAK DEMAND: ${value} kW`;
+    });
+
+    this.$peakCoord.subscribe(value => {
+      this.peaks[1].text = `PEAK DEMAND: ${value} kW`;
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.$dataReady.subscribe(ready => {
+      if (ready) {
+        this.zone.runOutsideAngular(() => {
+          const uncoordData = this.rtuData.map(record => ({
+            date: record.Time,
+            rtu: record['Bldg (kW) UnCoord'],
+            ev: record['Bldg + EV (kW) UnCoord'],
+            // base: record['Bldg Other (kW) UnCoord']
+          }));
+          const coordData = this.rtuData.map(record => ({
+            date: record.Time,
+            ev: record['Bldg (kW) Coord'],
+            ess: record['Bldg + EV + ESS (kW) Coord'],
+            // base: record['Bldg Other (kW) Coord']
+          }));
+
+          this.createChart('chart-uncoord', uncoordData);
+          this.createChart('chart-coord', coordData);
+        });
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.zone.runOutsideAngular(() => {
+      this.charts.forEach(chart => {
+        if (chart) {
+          chart.dispose();
+        }
+      });
+    });
+  }
+
+  hideCursor(): void {
+    this.charts.forEach(chart => {
+      chart.cursor.triggerMove({x: 0, y: 0}, 'none');
+    });
+    this.active = null;
+  }
+
+  setView(view: ViewType): void {
+    this.view = view;
+    localStorage.setItem('view', view);
+    // TODO update chart.yAxes.max, swap chart series'
+    this.update(this.timeIndex.current);
+  }
+
+  private update(currentTimeIndex: number): void {
+    this.updateBars(currentTimeIndex);
+    this.updatePeaks(currentTimeIndex);
+    this.updateStyles(currentTimeIndex);
+  }
+
+  private updateBars(currentTimeIndex: number): void {
+    this.bars.forEach(bar => {
+      bar.date = this.rtuData[currentTimeIndex].Time;
+    });
+  }
+
+  private updatePeaks(currentTimeIndex: number): void {
+    if (this.peaks.length === 2) {
+      if (this.view === 'rtu') {
+        this.peakUncoord.next(this.rtuData[currentTimeIndex]['Peak Running (kW) UnCoord'].toFixed(1));
+        this.peakCoord.next(this.rtuData[currentTimeIndex]['Peak Running (kW) Coord'].toFixed(1));
+      } else if (this.view === 'ev') {
+        this.peakUncoord.next(this.rtuData[currentTimeIndex]['Peak EV (kW) Coord'].toFixed(1));
+        this.peakCoord.next(this.rtuData[currentTimeIndex]['Peak ESS (kW) Coord'].toFixed(1));
+      }
+    }
+  }
+
+  private updateCursor(chartIndex: 0 | 1, point: { x: number; y: number }): void {
+    const height = this.charts[chartIndex].plotContainer.innerHeight;
+    const width = this.charts[chartIndex].plotContainer.innerWidth;
+    this.charts[chartIndex].cursor.triggerMove({
+      x: point.x * width,
+      y: height - (point.y * height)
+    }, 'soft');
+  }
+
+  private updateStyles(currentTimeIndex): void {
+    const data = this.rtuData[currentTimeIndex];
+    let left = ['base'];
+    let right = ['base'];
+
+    if (data['RTU On Count UnCoord'] > 0) {
+      if (data['RTU 1 State UnCoord'] === 1) {
+        left.unshift('rtu1');
+      }
+      if (data['RTU 2 State UnCoord'] === 1) {
+        left.unshift('rtu2');
+      }
+      if (data['RTU 3 State UnCoord'] === 1) {
+        left.unshift('rtu3');
+      }
+      if (data['RTU 4 State UnCoord'] === 1) {
+        left.unshift('rtu4');
+      }
+      if (data['RTU 5 State UnCoord'] === 1) {
+        left.unshift('rtu5');
+      }
+      if (data['RTU 6 State UnCoord'] === 1) {
+        left.unshift('rtu6');
+      }
+    }
+
+    if (data['RTU On Count Coord'] > 0) {
+      if (data['RTU 1 State Coord'] === 1) {
+        right.unshift('rtu1');
+      }
+      if (data['RTU 2 State Coord'] === 1) {
+        right.unshift('rtu2');
+      }
+      if (data['RTU 3 State Coord'] === 1) {
+        right.unshift('rtu3');
+      }
+      if (data['RTU 4 State Coord'] === 1) {
+        right.unshift('rtu4');
+      }
+      if (data['RTU 5 State Coord'] === 1) {
+        right.unshift('rtu5');
+      }
+      if (data['RTU 6 State Coord'] === 1) {
+        right.unshift('rtu6');
+      }
+    }
+
+    if (this.view === 'ev') {
+      left.unshift('charger');
+      right.unshift('charger');
+
+      if (data['Port 2 (kW)'] > 0) {
+        left.unshift('tesla-3');
+        right.unshift('tesla-3');
+      }
+      if (data['Port 1 (kW)'] > 0) {
+        left.unshift('tesla-x');
+        right.unshift('tesla-x');
+      }
+
+      right.unshift('storage');
+    }
+
+    left = left.map(img => `url("assets/images/${img}.png")`);
+    right = right.map(img => `url("assets/images/${img}.png")`);
+
+    const leftStyle = left.join(', ');
+    const rightStyle = right.join(', ');
+
+    if (this.leftStyle !== leftStyle) {
+      this.leftStyle = leftStyle;
+    }
+    if (this.rightStyle !== rightStyle) {
+      this.rightStyle = rightStyle;
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSliderModule } from '@angular/material/slider';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
